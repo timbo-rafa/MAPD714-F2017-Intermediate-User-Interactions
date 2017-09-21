782f27f4556bc7515b29965b2ff1d773f841686e
Student <student@setass-mac-mini-2.local>
2017-09-20 20:37:37 -0400

    Added Outlets and Actions

--- MAPD714-F2017-intermediate-User-interactions/ViewController.swift
+++ MAPD714-F2017-intermediate-User-interactions/ViewController.swift
@@ -3,7 +3,7 @@
  * Date: September 20, 2017
  * StudentID: 300962878
  * Description: Intermediate User Interactions Demo
- * Version 0.1 - Built Basic UI
+ * Version 0.2 - Added Outlets and Actions
  *
  */
 
@@ -11,10 +11,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // OUTLETS +++++++++++++++++++++++++++++++++++
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var sliderLabel: UISlider!
+    
+    // INHERITED METHODS ++++++++++++
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // ACTION METHODS ++++++++++++++++
+    @IBAction func onSliderChanged(_ sender: UISlider) {
+    
+    }
+    
+    @IBAction func onSegmentedControlChanged(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func onDoSomethingPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func onSwitchChange(_ sender: UISwitch) {
+    }
+    
 }
 
